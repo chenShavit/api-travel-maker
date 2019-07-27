d617cb9c39c9d622bb63eeeb6cd259b7574cb1cb
cShavit68 <ck20427@gmail.com>
2019-07-27 13:43:56 +0300

    change user&pass

--- consts.js
+++ consts.js
@@ -1,6 +1,6 @@
 module.exports = {
   consts: {
-    MLAB_KEY: 'mongodb://Pytheas:TravelMaker1@ds345937.mlab.com:45937/travel-maker'
+    MLAB_KEY: 'mongodb://chenShavit:eShavit1990S@ds345937.mlab.com:45937/travel-maker'
   }
 }
  
--- routes/tag.js
+++ routes/tag.js
@@ -2,11 +2,6 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const objectID = mongoose.Types.ObjectId;
 
-const sendError = require("./error");
-const sendSuccess = require('./success');
-
-const Trip = require('../db/trip');
-const User = require('../db/user');
 const Tag = require('../db/tag');
 
 
